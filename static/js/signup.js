7a82713b6f719d73435aa8deb40aa125b2c124e7
shivudu <sdeshi@osius.com>
2026-06-12 14:26:36 +0530

    update

--- static/js/signup.js
+++ static/js/signup.js
@@ -1,27 +1,31 @@
 var $DOM = $(document);
 
 $DOM.on('click', '#signup_submit', function() {
-
-	console.log("submit clicked");
-    data = {}
+    console.log("submit clicked");
+    var data = {};
     data["first_name"] = $(".fname").val();
     data["last_name"] = $(".lname").val();
     data["email"] = $(".email").val();
     data["phone_number"] = $(".phno").val();
 
-	$.ajax({
-		type: 'post',
+    $.ajax({
+        type: 'post',
+        contentType: 'application/json',
         data: JSON.stringify(data),
-		url: '/signup/validate',
-		success: function(result) {
+        url: '/signup/validate',
+        success: function(result) {
             console.log(result);
             if (result.success) {
-                window.location.href = "/login"
-            }
-            else {
+                window.location.href = "/login";
+            } else {
                 alertify.set('notifier', 'position', 'top-right');
                 alertify.error(result.message);
             }
-		}
-	});
+        },
+        error: function(xhr) {
+            alertify.set('notifier', 'position', 'top-right');
+            alertify.error("Signup failed. Please try again.");
+            console.error(xhr.responseText);
+        }
+    });
 });
